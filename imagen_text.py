131dd861a69eb5fe5986f467d2cc73688d0239a6
Danilo Michel <122659231+Dani10MPV@users.noreply.github.com>
2023-07-11 21:46:31 -0400

    Delete imagen_text.py

--- imagen_text.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-from pytesseract import *
-
-pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-img = Image.open("E:\Michel\Programacion\estudio python\Proyectos\Convertir imagen a texto\imagen2.png")
-resultado = pytesseract.image_to_string(img)
-print (resultado)
